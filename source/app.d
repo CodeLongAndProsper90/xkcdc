4b67538bbfce30a88bafece7d9919f55c0438924
Scott Little <swlittle7@gmail.com>
2020-10-14 18:00:44 -0500

    Added logging, fixed transcript

--- source/app.d
+++ source/app.d
@@ -8,20 +8,35 @@ import std.string;
 int main(string[] args)
 {
   bool export_json;
+  bool verbose;
   auto help = getopt(
                      args,
-                     "export-json", &export_json
+                     "export-json", &export_json, // Does the user want JSON?
+                     "verbose", &verbose, // Does the user want verbosity?
   );
-  if (help.helpWanted)
+
+  if (help.helpWanted) // -h flag invoked
     {
       defaultGetoptPrinter("Help for xkcdc:", help.options);
     }
+  
+  void log(string info)
+  {
+    if (verbose)
+      writeln(info);
+  }
+  
+  log("Downloading current comic"); // Give some info
+
+  // This is so we can tell if the comic exists or not
   string current = to!string(getContent("https://xkcd.com/info.0.json"));
-  auto comic = parseJSON(current);
+  auto comic = parseJSON(current); 
+
   if (args.length > 1)
     {
       int num = to!int(args[1]); // convert the first argument
       int exists = cast(int) comic["num"].integer;
+      log(format("Latest comic is %d, current comic is %d", exists, num));
       if (num > exists)
         // Comic out of range
         {
@@ -29,6 +44,7 @@ int main(string[] args)
           return 1;
         }
       string url = format("https://xkcd.com/%d/info.0.json", num);
+      log(format("Downloading JSON at %s", url));
       string req = to!string(getContent(url));
       comic = parseJSON(req);
     }
@@ -40,10 +56,10 @@ int main(string[] args)
     {
       
       string output = format(
-                            "Title: %s\nImage: %s\nTranscript: %s\nAlt: %s",
+                            "Title: %s\nImage: %s\n\nTranscript:\n===\n%s\n===\nAlt: %s",
                             text(comic["title"]),
                             replace(text(comic["img"]), "\\/", "/"),
-                            text(comic["transcript"]),
+                            replace(text(comic["transcript"]), "\\n", "\n"),
                             text(comic["alt"]),
                             );
       
